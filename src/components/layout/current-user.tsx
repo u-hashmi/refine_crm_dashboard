0d62eb4a7997917ce580c82695bbeab567f917a6
usamaH-dotdev <hash.design.develop@gmail.com>
2024-02-02 21:42:30 -0600

    Initial Commit

---
+++ src/components/layout/current-user.tsx
@@ -0,0 +1,69 @@
+import { useState } from 'react'
+import { Popover, Button } from 'antd'
+import CustomAvatar from '../custom-avatar'
+import {AccountSettings} from './account-settings'
+import { useGetIdentity } from '@refinedev/core'
+import { Text } from '../text'
+
+import type { User } from '@/graphql/schema.types';
+import { SettingOutlined } from '@ant-design/icons';
+
+export const CurrentUser = () => {
+    const [isOpen, setIsOpen] = useState(false)
+    const { data: user } = useGetIdentity<User>();
+
+    const content = (
+        <div style={{
+            display: 'flex',
+            flexDirection: 'column'
+        }}>
+            <Text
+                strong
+                style={{ padding: '12px 20px' }}
+            >
+                {user?.name}
+            </Text>
+            <div style={{
+                borderTop: '1px solid #d9d9d9',
+                padding: '4px',
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '4px'
+            }}>
+                <Button
+                    style={{ textAlign: 'left' }}
+                    icon={<SettingOutlined />}
+                    type="text"
+                    block
+                    onClick={() => setIsOpen(true)}
+                >
+                    Account Settings
+                </Button>
+            </div>
+        </div>
+    )
+
+    return (
+        <>
+            <Popover
+                placement='bottomRight'
+                trigger="click"
+                overlayInnerStyle={{ padding: 0 }}
+                overlayStyle={{ zIndex: 999 }}
+                content={content}
+            >
+                <CustomAvatar
+                    name={user?.name}
+                    src={user?.avatarUrl}
+                    size="default"
+                    style={{ cursor: 'pointer' }}
+                />
+            </Popover>
+            {user && (<AccountSettings 
+                opened={isOpen}
+                setOpened={setIsOpen}
+                userId={user.id}
+            />)}
+        </>
+    )
+}
---
+++ src/components/layout/header.tsx
@@ -0,0 +1,21 @@
+import { Layout, Space } from "antd"
+import { CurrentUser } from "./current-user"
+export const Header = () => {
+    const headerStyles: React.CSSProperties = {
+        backgroundColor: "#fff",
+        display: "flex",
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        padding: '0 24px',
+        position: 'sticky',
+        top: 0,
+        zIndex: 999
+    }
+    return (
+        <Layout.Header style={headerStyles}>
+            <Space align="center" size="middle">
+                <CurrentUser />
+            </Space>
+        </Layout.Header>
+    )
+}
